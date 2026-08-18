bb81721691f56e0ec5f558637de543ec8d612258
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-18 11:19:49 +0300

    Question01

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part01
+
+            #region Question01
+            // a) Abstraction means hiding complex implementation details and showing only the important information.
+
+           // b) Because it reduces complexity and helps organize the code by focusing on what an object does rather than how it does it.
+            #endregion
+
+            #endregion
         }
     }
   
