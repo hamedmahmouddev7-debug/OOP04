b8e1a1bc1122ad35e3be2c2341d7c916abce7228
hamedmahmouddev7-debug <hamedmahmoud.dev7@gmail.com>
2026-08-18 11:22:17 +0300

    Question02

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OOP04
+﻿using System.Diagnostics.Contracts;
+
+namespace OOP04
 {
     class Program
     {
@@ -9,10 +11,22 @@
             #region Question01
             // a) Abstraction means hiding complex implementation details and showing only the important information.
 
-           // b) Because it reduces complexity and helps organize the code by focusing on what an object does rather than how it does it.
+            // b) Because it reduces complexity and helps organize the code by focusing on what an object does rather than how it does it.
             #endregion
 
+            #region Question02
+            /*a) Difference:
+
+            Abstract Class: Can contain abstract methods, normal methods, fields, and constructors.
+            Interface: Defines a contract that a class must follow.
+
+            b) Choose an Interface when different classes need to share the same behavior, even if they are not related by inheritance.
+
+            c)  A class cannot inherit from multiple abstract classes.
+                A class can implement multiple interfaces.
+            */
             #endregion
+        #endregion
         }
     }
   
